87187b9e7e69818fd0782ecb7e3b85b6cbbdddde
VaynePeng <vaynepeng@gmail.com>
2024-03-05 15:08:05 +0800

    chore: 删除非必要文件

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './assets/styles/base.css'
+import './global.css'
 
 const App = () => {
   return <div className="w-screen h-screen bg-red-50 select-none text-5xl font-mono text-purple-300 backdrop-blur-md flex items-center justify-center">Hello,Vayne.</div>
