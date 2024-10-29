eac7a7e4611ca35520514f49675594f963658c48
VaynePeng <vaynepeng@gmail.com>
2024-10-29 16:01:22 +0800

    refactor: 删除不再使用的菜单和待办组件

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { ToastContainer } from 'react-toastify'
 import Slogan from '@/components/Slogan'
-import Menu from '@/components/Menu'
 
 import './global.css'
 
@@ -10,7 +9,6 @@ const App = () => {
   return (
     <div className="w-screen h-screen bg-red-50 relative select-none">
       <Slogan slogan="Welcome." />
-      <Menu />
     </div>
   )
 }
